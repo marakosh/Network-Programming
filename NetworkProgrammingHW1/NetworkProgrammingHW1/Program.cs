20564f0652df183700bc96be51da337b9c3bb177
marakosh <marat.garifulin160@gmail.com>
2023-03-19 21:20:53 +0400

    HW

---
+++ NetworkProgrammingHW1/NetworkProgrammingHW1/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+
+            string uri = $"weather?q=Baku&appid=5036d6148020b0e67dbcb0434da77e98";
+
+            HttpResponseMessage response = await client.GetAsync(uri);
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseString);
+        }
+    }
+}
